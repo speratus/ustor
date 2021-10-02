6be54363b56eb4cd39c36f7170c5c8e060530ce7
Andrew Luchuk <andrew.luchuk@outlook.com>
2021-10-01 23:19:44 -0400

    begins adding a client trait

--- src/types.rs
+++ src/types.rs
@@ -1,5 +1,6 @@
 pub mod types {
     use std::hash::Hash;
+    use std::net::{SocketAddr, TcpStream};
 
     pub trait Udb {
         fn insert_or_update<T>(&mut self, key: String, val: T) -> Option<T>;
@@ -9,6 +10,16 @@ pub mod types {
         fn get<T>(&self, key: String) -> Option<T>;
 
         fn key_exists(&self, key: String) -> bool;
+    }
+
+    pub trait Client {
+        fn get_remote_addr(&self) -> &SocketAddr;
+
+        fn get_stream(&self) -> &TcpStream;
+
+        fn get_stream_mutable(&self) -> &mut TcpStream;
+
+        fn subscribed_channels(&self) -> &Vec<String>;
     }
 
 }
